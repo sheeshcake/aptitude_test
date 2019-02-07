2b2d6814c13c26a4bba19e984eafff371166afbc
wendale1231 <44239838+wendale1231@users.noreply.github.com>
2019-02-08 00:00:53 +0800

    Updated Login
    *Added some Aesthetics

--- login.php
+++ login.php
@@ -16,21 +16,28 @@
 				<input type="radio" onclick="student_entry();" name="department" id="student" value="student" required>I'm a fucking Student
 				<input type="radio" onclick="other_entry();" id="other" name="department" value="other" required>I'm a fucking Dean or Teacher
 			</p>
-			<p>Password<input type="password" name="password" id="password" required></p>
-			<button type="submit" id="submit">Log-in</button>
+			<p id="pass_entry"></p>
+			<p id="login_btn"></p>
 		</form>
 	</center>
 
 
 	<script>
+		var pass_entry = false;
 		function student_entry(){
-
 			document.getElementById("entry").innerHTML = "ID-Number: C<input type='number' min='01' max='99'style='width:30px' name='yr'>-<input type='number' style='width:50px' name='num'>";
+			pass_entry = true;
+			add_pass();
 		}
 		function other_entry(){
 			document.getElementById("entry").innerHTML = "Username<input type='text' id='username' name='username'>";
+			pass_entry = true;
+			add_pass();
 		}
-
+		function add_pass(){
+			document.getElementById("pass_entry").innerHTML = "Password<input type='password' name='password' id='password' required>"
+			document.getElementById("login_btn").innerHTML = "<button type='submit' id='submit'>Log-in</button>"
+		}
 
 	</script>
 </body>
